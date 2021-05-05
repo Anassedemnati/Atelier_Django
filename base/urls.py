8d9a276e3f8b9775938878de05ccf87454f9326a
Anassedemnati <DEMNATI_ANASSE@emsi-edu.ma>
2021-05-05 16:43:53 +0000

    static integrate bootstrap

--- base/urls.py
+++ base/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.IndexView.as_view(),name="home"),
     path('products/', views.ProductView.as_view(),name="products"),
+    
     
 
 ]
--- base/views.py
+++ base/views.py
@@ -3,15 +3,14 @@ from django.http import HttpResponse
 from django.views import View
 
 # Create your views here.
-# def home_page(request):
-#     return render(request,"home.html")
 
-# def product(request):
-#     return render(request,"products.html")
 class ProductView(View):
     def get(self,request):
         products=['hp','mac','acer','dell','pak']
         return render(request,"products.html",{'products':products})
 
-# def about(request):
-#     return render(request,"about.html")
+
+
+class IndexView(View):
+    def get(self,request):
+        return render(request,"index.html",{})
