78aff1617a26bd8b5f229ae5382b686cd6306fa9
Anassedemnati <DEMNATI_ANASSE@emsi-edu.ma>
2021-05-19 17:15:00 +0100

    Atelier 4 end form and model && update file name products to product_form

--- base/views.py
+++ base/views.py
@@ -1,44 +1,49 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
 from .models import *
 from .form import *
 
+
 # Create your views here.
 class ProductlistView(View):
     def get(self, request):
-        products=Product.objects.all()
-        return render(request,"product_list.html",{'products':products} )
+        products = Product.objects.all()
+        return render(request, "product_list.html", {'products': products})
+
 
 class CategorylistView(View):
     def get(self, request):
-        Categorys=Category.objects.all()
-        return render(request,"category_list.html",{'Categorys':Categorys} )
+        Categorys = Category.objects.all()
+        return render(request, "category_list.html", {'Categorys': Categorys})
 
+
+def post(request):
+    form = ProductForm(request.POST, request.FILES)
+    if form.is_valid():
+        form.save()
+    return redirect("productslist")
 
 
 class ProductView(View):
-    def get(self,request):
-        form=ProductForm()
-        
-        return render(request,"products.html",{'form':form})
-    def post(self,request):
-        form=ProductForm(request.POST,request.FILES)
-        if form.is_valid():
-            form.save()
-        return redirect("productslist")
+    def get(self, request):
+        form = ProductForm()
+
+        return render(request, "product_form.html", {'form': form})
+
+
 class CategoryView(View):
-    def get(self,request):
-        form=CategoryForm()
-        return render(request,"category_form.html",{'form':form})
-    def post(self,request):
-        form=CategoryForm(request.POST,request.FILES)
+    def get(self, request):
+        form = CategoryForm()
+        return render(request, "category_form.html", {'form': form})
+
+    def post(self, request):
+        form = CategoryForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
         return redirect("Categorylist")
 
 
-
 class IndexView(View):
-    def get(self,request):
-        return render(request,"index.html",{})
+    def get(self, request):
+        return render(request, "index.html", {})
