a732700b96e7fbb34c28bc7d5035124cb565ae9e
Anassedemnati <DEMNATI_ANASSE@emsi-edu.ma>
2021-05-19 16:58:34 +0100

    Atelier 4 end form and model

--- base/models.py
+++ base/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django import forms
 # Create your models here.
 class Category(models.Model):
-    name=models.CharField(max_length=100)
-    description=models.TextField()
+    name=models.CharField(max_length=100,null=True)
+    description=models.TextField(null=True)
     def __str__(self):
         return self.name
 class Product(models.Model):
@@ -13,6 +13,6 @@ class Product(models.Model):
     prce=models.FloatField()
     contInstock=models.IntegerField()
     image=models.ImageField(upload_to="images/")
-    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE,null=True)
     def __str__(self):
         return self.name
